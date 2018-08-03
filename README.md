8adcd1e4764cf602ac74cce303fe679c153d2613
percy_tataje <percytataje@gmail.com>
2018-08-03 14:51:49 -0500

    feat(server): se añadio la funcionalidad de json-server

--- README.md
+++ README.md
@@ -4,13 +4,17 @@ Base para inicializar un proyecto en react:
 # Configurado para usar Typescript, Jest(test)
 
 # Para levantar el servidor
-Ejecutar desde la raiz el comando yarn server
+Ejecutar desde la raiz el comando 
+yarn start
 
 # Para construir el bundle
 Ejecutar desde la raiz el comando yarn prod
 
 # Para construir el bundle y levantar el servidor
-Ejecutar desde la raiz el comando yarn start
+Ejecutar desde la raiz el comando yarn buildprod
+
+# Para levantar el json server
+Ejecutar desde la raiz el comando yarn server
 
 
 
---
+++ jsonserver/index.js
@@ -0,0 +1,26 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(__dirname + '/db.json');
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+
+server.use(jsonServer.rewriter({
+  '/empleados/emp': '/empleados',
+}))
+
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  next()
+})
+
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running!');
+  console.log('Source:', router.db.source);
+});
